5fb487bae8eba3a9586aa028cb07337575d024aa
Luis Adrian Gonzalez Benavides <adrian.and1@gmail.com>
2011-08-12 15:44:20 -0430

    dia 3 commit
    algo en vida y poner nombre a las tablas

--- src/com/jswitch/siniestros/modelo/dominio/EtapaSiniestro.java
+++ src/com/jswitch/siniestros/modelo/dominio/EtapaSiniestro.java
@@ -13,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Version;
 
 /**
@@ -20,6 +21,7 @@ import javax.persistence.Version;
  * @author Personal
  */
 @Entity
+@Table(name = "SINI_EtapaSiniestro")
 public class EtapaSiniestro extends BeanVO implements Serializable, Auditable {
 
     /**
